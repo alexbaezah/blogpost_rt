a82bf0599f5d6913aff45f30a791a2e3b1a606d2
alex <alcebaeza@gmail.com>
2019-09-03 17:27:17 -0400

    blogpost en tiempo real

---
+++ config/initializers/pusher.rb
@@ -0,0 +1,8 @@
+require 'pusher'
+
+Pusher.app_id = '855248'
+Pusher.key = '2b55c01e0a7bcbe06f9c'
+Pusher.secret = '50d9fb9bedbe1d54b885'
+Pusher.cluster = 'us2'
+Pusher.logger = Rails.logger
+Pusher.encrypted = true
